1058020486f893542a299fd16bbdd8446e07b006
Gabriel Martinez <gabriel.emv411@gmail.com>
2023-05-10 18:23:44 -0600

    Add: Initialized Base Spring Boot App

---
+++ villaticket/src/main/java/com/bancrabs/villaticket/VillaticketApplication.java
@@ -0,0 +1,13 @@
+package com.bancrabs.villaticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillaticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VillaticketApplication.class, args);
+	}
+
+}
---
+++ villaticket/src/test/java/com/bancrabs/villaticket/VillaticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancrabs.villaticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillaticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
